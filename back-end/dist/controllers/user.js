b93d881b0992e799f302ff5ab3de03fddfb3b9d2
TomatoSlasher <oahwidy7@gmail.com>
2021-10-13 20:05:38 +0300

    signup password encryption and encrypted  login passowrd validation

--- back-end/dist/controllers/user.js
+++ back-end/dist/controllers/user.js
@@ -11,6 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 // const { validationResult } = require("express-validator/check");
 const express_validator_1 = require("express-validator");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 exports.loginUser = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const errors = (0, express_validator_1.validationResult)(req);
@@ -22,54 +23,62 @@ exports.loginUser = (req, res, next) => __awaiter(void 0, void 0, void 0, functi
     const email = req.body.email;
     const password = req.body.password;
     const userEmail = yield User.findOne({ email: email });
-    console.log(userEmail);
+    const doMatch = userEmail
+        ? yield bcrypt.compare(password, userEmail.password)
+        : null;
     if (!userEmail) {
         return res.status(200).json({
             message: "user doesn't exist",
         });
     }
-    if (userEmail.email != email || userEmail.password != password) {
+    if (userEmail.email != email || !doMatch) {
         return res.status(200).json({
             message: "Invalid E-mail or Password",
         });
     }
-    if (userEmail.email == email && userEmail.password == password) {
+    if (userEmail.email == email && doMatch) {
         return res.status(200).json({
             message: "Logged In",
         });
     }
 });
 exports.createUser = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const errors = (0, express_validator_1.validationResult)(req);
-    if (!errors.isEmpty()) {
-        const error = new Error("Validation failed, entered data is incorrect.");
-        error.statusCode = 422;
-        throw error;
-    }
-    const email = req.body.email;
-    const password = req.body.password;
-    const userEmail = yield User.findOne({ email: email });
-    if (userEmail) {
-        return res.status(200).json({
-            message: "user already exists",
-        });
-    }
-    const user = new User({
-        email: email,
-        password: password,
-    });
-    user
-        .save()
-        .then(() => {
-        res.status(200).json({
-            message: "user created",
-            res: { email: req.body.email, passowrd: req.body.password },
-        });
-    })
-        .catch((err) => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
+    try {
+        const errors = (0, express_validator_1.validationResult)(req);
+        if (!errors.isEmpty()) {
+            const error = new Error("Validation failed, entered data is incorrect.");
+            error.statusCode = 422;
+            throw error;
         }
-        next(err);
-    });
+        const email = req.body.email;
+        const password = req.body.password;
+        const encryptedPassowrd = yield bcrypt.hash(password, 12);
+        const userEmail = yield User.findOne({ email: email });
+        if (userEmail) {
+            return res.status(200).json({
+                message: "user already exists",
+            });
+        }
+        const user = new User({
+            email: email,
+            password: encryptedPassowrd,
+        });
+        user
+            .save()
+            .then(() => {
+            res.status(200).json({
+                message: "user created",
+                res: { email: req.body.email },
+            });
+        })
+            .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
--- back-end/src/controllers/user.ts
+++ back-end/src/controllers/user.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Response } from "express";
 // const { validationResult } = require("express-validator/check");
 import { validationResult } from "express-validator";
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.loginUser = async (req: any, res: Response, next: NextFunction) => {
@@ -14,20 +16,21 @@ exports.loginUser = async (req: any, res: Response, next: NextFunction) => {
   }
   const email = req.body.email;
   const password = req.body.password;
-
   const userEmail = await User.findOne({ email: email });
-  console.log(userEmail);
+  const doMatch = userEmail
+    ? await bcrypt.compare(password, userEmail.password)
+    : null;
   if (!userEmail) {
     return res.status(200).json({
       message: "user doesn't exist",
     });
   }
-  if (userEmail.email != email || userEmail.password != password) {
+  if (userEmail.email != email || !doMatch) {
     return res.status(200).json({
       message: "Invalid E-mail or Password",
     });
   }
-  if (userEmail.email == email && userEmail.password == password) {
+  if (userEmail.email == email && doMatch) {
     return res.status(200).json({
       message: "Logged In",
     });
@@ -35,39 +38,43 @@ exports.loginUser = async (req: any, res: Response, next: NextFunction) => {
 };
 
 exports.createUser = async (req: any, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error: any = new Error(
-      "Validation failed, entered data is incorrect."
-    );
-    error.statusCode = 422;
-    throw error;
-  }
-  const email = req.body.email;
-  const password = req.body.password;
-
-  const userEmail = await User.findOne({ email: email });
-  if (userEmail) {
-    return res.status(200).json({
-      message: "user already exists",
-    });
-  }
-  const user = new User({
-    email: email,
-    password: password,
-  });
-  user
-    .save()
-    .then(() => {
-      res.status(200).json({
-        message: "user created",
-        res: { email: req.body.email, passowrd: req.body.password },
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error: any = new Error(
+        "Validation failed, entered data is incorrect."
+      );
+      error.statusCode = 422;
+      throw error;
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+    const encryptedPassowrd = await bcrypt.hash(password, 12);
+    const userEmail = await User.findOne({ email: email });
+    if (userEmail) {
+      return res.status(200).json({
+        message: "user already exists",
       });
-    })
-    .catch((err: any) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    }
+    const user = new User({
+      email: email,
+      password: encryptedPassowrd,
     });
+    user
+      .save()
+      .then(() => {
+        res.status(200).json({
+          message: "user created",
+          res: { email: req.body.email },
+        });
+      })
+      .catch((err: any) => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+      });
+  } catch (error) {
+    console.log(error);
+  }
 };
